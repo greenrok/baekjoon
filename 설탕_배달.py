021315ef26059f15656e46e02fa0bf9d9a6bcc3e
DaeRokKim <kdr0112@gmail.com>
2022-05-04 00:07:35 +0900

    220503 class2

--- 설탕_배달.py
+++ 설탕_배달.py
@@ -1,15 +1,35 @@
+# n = int(input())
+
+# 처음 짠 코드 맞긴 한데 틀림
+# if (n % 5) % 3 == 0:
+#     a = n // 5
+#     b = (n % 5) // 3
+#     print(a+b)
+# elif (n % 5) % 3 != 0 and n % 3 != 0:
+#     if (n // 5) != 0 and ((n % 5) + 5) % 3 == 0:
+#         a = n // 5 - 1
+#         b = ((n % 5) + 5) // 3
+#         print(a+b)
+#     else:
+#         print(-1)
+# elif (n % 5) % 3 != 0 and n % 3 == 0:
+#     print(n//3)
+# else:
+#     a = (n // 5) - 1
+#     b = ((n % 5) + 5) // 3
+#     print(a+b)
+
+
 n = int(input())
 
-if n % 5 < 3:
-    a = (n // 5) - 1
-    b = ((n % 5) + 3) // 3
-    print(a+b)
-elif n // 5 == 0 or ((n % 5) % 3 != 0):
-    print(-1)
+bag = 0
+while n >= 0:
+    if n % 5 == 0:
+        bag += n // 5
+        print(bag)
+        break
+    n -= 3
+    bag += 1
 else:
-    a = n // 5
-    b = ((n % 5) // 3)
-    print(a+b)
-
-
-    
+    print(-1)
+        
