5bc60e5930f434c6f4eda63db860acc53841fe18
greenrok <kdr0112@gmail.com>
2022-09-09 23:08:59 +0900

    220909

---
+++ cpp/class2/ACM_호텔.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int t, h, w, n;
+   cin >> t;
+
+   for (int i = 0; i < t; i++)
+   {
+      cin >> h >> w >> n;
+
+      int h1, w1;
+
+      h1 = n % h;
+      w1 = n / h;
+
+      if (h1 > 0)
+      {
+         w1++;
+      }
+      else
+      {
+         h1 = h;
+      }
+
+      cout << h1 * 100 + w1 << '\n';
+   }
+
+   return 0;
+}
