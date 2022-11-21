a176b8635e79414e32403a22375f1a5f3c2d6079
rok <rok@rokui-MacBookPro.local>
2022-11-21 22:36:55 +0900

    221121 solved

---
+++ cpp/basic/명령_프롬프트.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N, i;
+    string str[50];
+    char c;
+
+    cin >> N;
+
+    for (i = 0; i < N; i++)
+    {
+        cin >> str[i];
+    }
+
+    if(N==1) {
+        for(i=0; i<str[0].length(); i++) {
+            cout << str[0][i];
+        }
+    } else {
+        for(i=0; i<str[0].length(); i++) {
+            c = str[0][i];
+            for(int j=0; j<N; j++) {
+                if(c != str[j][i]) {
+                    c = '?';
+                    break;
+                }
+            }
+            cout << c;
+        }
+    }
+
+    return 0;
+}
