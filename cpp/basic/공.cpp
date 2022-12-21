9cad958373ded91fab58b092fde4a9edd91239cc
greenrok <kdr0112@gmail.com>
2022-12-21 17:37:28 +0900

    221221

---
+++ cpp/basic/공.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int N;
+   bool a = true, b = false, c = false;
+   int x, y;
+
+   cin >> N;
+
+   for (int i = 0; i < N; i++)
+   {
+      cin >> x >> y;
+   }
+
+   return 0;
+}
